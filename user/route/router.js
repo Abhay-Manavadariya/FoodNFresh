245b91274e2879be134ea3324112fe31a8ec92b9
abhay manavadariya <abhay.manavadariya@emgage.work>
2024-06-06 18:56:43 +0530

    file name changes

--- user/route/router.js
+++ user/route/router.js
@@ -2,12 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { check } = require("express-validator");
 const multer = require("multer");
-const jwt = require("jsonwebtoken");
 const { guest } = require("../middleware/guest");
 require("dotenv").config();
 
-const user = require("../models/user");
-const { homePage } = require("../controller/homeController");
+const { homePage } = require("../controller/homecontroller");
 const {
   login,
   register,
